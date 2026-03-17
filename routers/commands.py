a3034c88ed4d0b9c7d5b9e20a4583ebfcdfad861
zhenya194 <zhenya.pro1753@gmail.com>
2026-03-17 18:41:29 +0200

    Add /myname command

--- routers/commands.py
+++ routers/commands.py
@@ -11,10 +11,18 @@ async def cmd_start(message: types.Message):
 async def cmd_start(message: types.Message):
     await message.answer(f"Your id: <code>{message.from_user.id}</code>", parse_mode="HTML")
 
+@router.message(Command("myname"))
+async def cmd_start(message: types.Message):
+    await message.answer(f"Your name:\n\n"
+                         f"Full name: <code>{message.from_user.full_name}</code>\n"
+                         f"First name: <code>{message.from_user.first_name}</code>\n"
+                         f"Last name: <code>{message.from_user.last_name}</code>\n", parse_mode="HTML")
+
 @router.message(Command("help"))
 async def cmd_help(message: types.Message):
     return await message.answer(f"Commands for <b>What is my id bot</b>:\n\n"
         f"/start - restart bot\n"
         f"/help - show this message\n"
-        f"/myid - show your id",
+        f"/myid - show your id\n"
+        f"/myname - show your name",
         parse_mode="HTML")
