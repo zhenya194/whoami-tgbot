6e1cf9f2e80127e7a6c17d9bfcf52ccb8f8a1856
zhenya194 <zhenya.pro1753@gmail.com>
2026-04-13 18:27:56 +0300

    Add ping to main.py

--- main.py
+++ main.py
@@ -3,6 +3,26 @@ import logging
 from aiogram import Bot, Dispatcher
 from config import config
 from routers import commands
+from aiohttp import web
+import os
+
+
+bot = Bot(token=config.bot_token.get_secret_value())
+dp = Dispatcher()
+
+# --- ping router ---
+async def ping(request):
+    return web.Response(text="ok")
+async def start_web():
+    app = web.Application()
+    app.router.add_get("/ping", ping)
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+
+    port = int(os.environ.get("PORT", 10000))
+    site = web.TCPSite(runner, "0.0.0.0", port)
+    await site.start()
 
 async def main():
     logging.basicConfig(level=logging.INFO)
@@ -13,8 +33,12 @@ async def main():
     dp.include_routers(commands.router)
 
     print("Bot successfully opened!")
+    await start_web()
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+
+asyncio.run(main())
+
 
 if __name__ == "__main__":
     asyncio.run(main())
