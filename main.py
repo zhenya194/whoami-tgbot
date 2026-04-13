9de7295e62ed3beaab4d455ba126b950f9f6f2de
zhenya194 <zhenya.pro1753@gmail.com>
2026-04-13 18:34:46 +0300

    Update ping in main.py

--- main.py
+++ main.py
@@ -1,36 +1,13 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
+from aiohttp import web
 from config import config
 from routers import commands
-from aiohttp import web
 import os
+import time
 
-
-bot = Bot(token=config.bot_token.get_secret_value())
-dp = Dispatcher()
-
-# --- ping router ---
-async def ping(request):
-    return web.Response(text="ok")
-async def start_web():
-    app = web.Application()
-    app.router.add_get("/ping", ping)
-
-    runner = web.AppRunner(app)
-    await runner.setup()
-
-    port = int(os.environ.get("PORT", 10000))
-import asyncio
-import logging
-import os
-
-from aiogram import Bot, Dispatcher
-from aiohttp import web
-
-from config import config
-from routers import commands
-
+time.sleep(85)
 
 # --- ping endpoint ---
 async def ping(request):
